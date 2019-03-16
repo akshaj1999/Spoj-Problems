90429fac6704402f7d30d4a87202124476f62a88
Akshaj Juneja <43871535+akshaj1999@users.noreply.github.com>
2019-03-16 14:00:31 +0530

    Add files via upload

---
+++ FCTRL2 - Small factorials.java
@@ -0,0 +1,69 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*; 
+class Demo {
+
+static BigInteger factorial(BigInteger n) 
+{ 
+    if (n.compareTo(new BigInteger("0"))==0) 
+    return new BigInteger("1"); 
+    return n.multiply(factorial(n.subtract(new BigInteger("1")))); 
+} 
+
+    public static void main(String[] args) throws Exception {
+        FastScanner cin = new FastScanner(System.in);
+        int t = cin.nextInt();
+        StringBuffer ans = new StringBuffer();
+        while(t-->0)
+        {
+           String str = cin.nextLine();
+           ans.append(factorial(new BigInteger(str))+"\n");
+        }
+     System.out.print(ans);
+}
+
+        
+    static class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+ 
+        public FastScanner(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+ 
+            return tokenizer.nextToken("\n");
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+         
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+    }
+  } 
